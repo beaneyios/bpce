8a474b8bd182251c1ad2e39b0fb0652205eeaf47
Tom the Don <tom.harding@pagesuite.com>
2018-03-30 17:29:21 +0100

    Added logic for method requests

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
 <?php
 
 include("Data/MySQLArticleService.php");
+init();
 
-$service = new MySQLArticleService();
+function init() {
+    $service = new MySQLArticleService();
+    $method = $_SERVER['REQUEST_METHOD'];
 
-$newArticle;
+    switch ($method) {
+        case 'GET':
+            echo json_encode($service->get());
+            break;
+        case 'POST':
+            $postMalone = json_decode(file_get_contents('php://input'), true);
+            
+            $newArticle = new Article();
+            $newArticle->Headline = $postMalone['Headline'];
+            $newArticle->Subheadline = $postMalone['Subheadline'];
+            $newArticle->ShareLink = $postMalone['ShareLink'];
 
-foreach($service->get() as $article) {
-    $newArticle = $article;
+            echo $service->create($newArticle);
+            break;
+        case 'PUT':
+            echo 'put';
+            break;
+        case 'DELETE':
+            echo 'delete';
+            break;
+        default:
+            echo 'default';
+            break;
+    }
 }
 
-
-echo $service->create($newArticle);
- 
 ?>
